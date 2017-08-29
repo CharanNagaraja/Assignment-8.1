9a53bcf8a5e46199faf4e43c23b46dd4382a9e0f
CN045696 <CN045696@W1571417.northamerica.cerner.net>
2017-08-29 21:38:05 +0530

    commit

---
+++ src/com/thread/java/thread.java
@@ -0,0 +1,19 @@
+package com.thread.java;
+
+import com.thread.java.timer;
+
+public class thread {
+public static void main(String[] args) {
+	
+timer t=new timer();
+	
+	t.start();
+	
+
+	
+	
+	
+
+}
+	
+}
